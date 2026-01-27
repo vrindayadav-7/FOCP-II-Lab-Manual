a95175dc6da27a6ab697fba4728ad278ad162d43
Vrinda <yvrinda67@gmail.com>
2026-01-27 22:29:10 +0530

    Update and rename Que1.cpp to 2026-01-27.cpp

--- 2026-01-27.cpp
+++ 2026-01-27.cpp
@@ -1,4 +1,4 @@
-//Solution to accept marks of 3 students and compute their average.
+//que.1- Solution to accept marks of 3 students and compute their average.
 
 #include<iostream>
 using namespace std;
@@ -13,4 +13,5 @@ avg = (marks1+ marks2+ marks3)/3;
 cout<<"avg marks of three students: "<<avg<<endl;
 
 return 0;
+
 }
