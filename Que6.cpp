179aed02dcb29decaba4fa1c652a26425571bd94
Vrinda <yvrinda67@gmail.com>
2026-01-27 22:37:17 +0530

    Update Que6.cpp

--- Que6.cpp
+++ Que6.cpp
@@ -1,3 +1,6 @@
+//Date- 27-01-2026
+//Solution to accept the number of employees and their basic salary and compute bonus and net salary.
+
 #include <iostream>
 using namespace std;
 
@@ -18,6 +21,5 @@ int main() {
         cout << "Bonus (12%): " << bonus << endl;
         cout << "Net Salary: " << netSalary << endl;
     }
-
     return 0;
 }
