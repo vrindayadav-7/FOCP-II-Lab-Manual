c004d924bfe35be575d09a8eee4fa315a21d9b74
Vrinda <yvrinda67@gmail.com>
2026-03-17 18:22:03 +0530

    Refactor area class to improve structure and functionality
    Refactored area class to use methods for setting data and calculating area.

--- 17-02-26/area.cpp
+++ 17-02-26/area.cpp
@@ -1,27 +1,40 @@
+// area of triangle class
+
 #include <iostream>
 using namespace std;
 // we put data and functions in class
 class area
 {
-private:
+protected:
     int base, height;
-    int triarea;
-    
 
 public:
-    area(int b, int h){ //constructor
-        base=b;
-        height=h;
-        triarea= 0.5*b*h;
-                cout<<"area of traingle is "<< triarea;
+    float area;
+
+    void data(int b, int h)
+    {
+        base = b;
+        height = h;
+    }
+    void areaTri()
+    {
+        area = 0.5 * base * height;
+        
     }
 };
 
 int main()
 {
-     // using class
-    area(40,20); // using constructor 
+    area a1; // using class
+    int b, h; // b is base and h is height
+    cout<<"enter base of triangle: ";
+    cin>>b;
+    cout<<"enter height of triangle: ";
+    cin>>h;
 
+    a1.data(b, h); // will give value of base and height to data function(class)
+    a1.areaTri(); 
 
+    cout << "area of triangle is: " << a1.area << endl;
     return 0;
-}
+} 17
